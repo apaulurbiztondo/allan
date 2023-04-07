fb9dd306bbba2e2f2619e437ac8ce1477c8c746f
allan <allan@192.168.1.16>
2023-04-07 20:55:17 +0800

    feat: uses lombok @Slf4j for logger

--- allan/src/main/java/com/example/allan/users/UserController.java
+++ allan/src/main/java/com/example/allan/users/UserController.java
@@ -2,8 +2,6 @@ package com.example.allan.users;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.allan.exceptions.ResourceNotFoundException;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
-
-	private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	private final UserService userService;
 
@@ -33,49 +32,49 @@ public class UserController {
 	@GetMapping
 	@Cacheable
 	public List<User> getAllUsers() {
-		logger.info("Getting all users");
+		log.info("Getting all users");
 		return userService.getAllUsers();
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<User> getUserById(@PathVariable Long id) {
-		logger.info("Getting user with id: {}", id);
+		log.info("Getting user with id: {}", id);
 		try {
 			User user = userService.getUserById(id);
 			return ResponseEntity.ok().body(user);
 		} catch (ResourceNotFoundException e) {
-			logger.error(e.getMessage());
+			log.error(e.getMessage());
 			return ResponseEntity.notFound().build();
 		}
 	}
 
 	@PostMapping
 	public ResponseEntity<User> createUser(@RequestBody User user) {
-		logger.info("Creating new user");
+		log.info("Creating new user");
 		User createdUser = userService.createUser(user);
 		return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
 	}
 
 	@PutMapping("/{id}")
 	public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
-		logger.debug("Updating user with id: {}", id);
+		log.info("Updating user with id: {}", id);
 		try {
 			User updatedUser = userService.updateUser(id, user);
 			return ResponseEntity.status(HttpStatus.CREATED).body(updatedUser);
 		} catch (ResourceNotFoundException e) {
-			logger.error(e.getMessage());
+			log.error(e.getMessage());
 			return ResponseEntity.notFound().build();
 		}
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-		logger.debug("Deleting user with id: {}", id);
+		log.info("Deleting user with id: {}", id);
 		try {
 			userService.deleteUser(id);
 			return ResponseEntity.noContent().build();
 		} catch (ResourceNotFoundException e) {
-			logger.error(e.getMessage());
+			log.error(e.getMessage());
 			return ResponseEntity.notFound().build();
 		}
 	}
