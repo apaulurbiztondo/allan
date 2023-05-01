912866f5ff8222ff165cf052b8652ded67c774e0
allan <allanpaulourbiztondo@gmail.com>
2023-05-01 18:50:48 +0800

    chore: add SQL and JDBC bind logging

--- src/main/java/com/example/demo/users/User.java
+++ src/main/java/com/example/demo/users/User.java
@@ -15,6 +15,7 @@ import lombok.NoArgsConstructor;
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
 
 	@Column(name = "name")
